349df0765748e683df59d5e20a99ce5bf75d151a
Sanskruti Kolgane <96910867+Sansgithub22@users.noreply.github.com>
2023-06-11 22:59:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var element;
+element = document.getElementsByTagName("ul");
+
+console.log(element);
+
+// element=document.getElementsByClassName("para1").innerText = "hey";
+// element = document.getElementsByClassName("para1").innerHTML;
+
+element = document.querySelector(".list");
+console.log(element);
